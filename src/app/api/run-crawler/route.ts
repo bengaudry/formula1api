c61896277c80feffd883c2fd980d0a12cf76d8bd
bengaudry <bengaudry@outlook.fr>
2024-06-14 22:21:10 +0200

    Lots of upgrades

--- src/app/api/run-crawler/route.ts
+++ src/app/api/run-crawler/route.ts
@@ -5,7 +5,7 @@ import { cwd } from "process";
 
 // Fonction pour gérer les requêtes POST
 export async function GET(req: NextRequest) {
-  console.info(cwd());
+  console.info("Current working directory", cwd());
   try {
     const params = req.nextUrl.searchParams;
     const crawlerType = params.get("crawler");
@@ -14,27 +14,50 @@ export async function GET(req: NextRequest) {
     const id = params.get("id");
 
     if (!crawlerType || !year || !location || !id) {
-      return NextResponse.json({ error: "Missing parameters (expected : year, location, id, crawler)" }, { status: 400 });
+      return NextResponse.json(
+        {
+          error: "Missing parameters (expected : year, location, id, crawler)",
+        },
+        { status: 400 }
+      );
     }
 
-    exec("python full_weekend.py 2024 canada 1237", { cwd: path.join(cwd(), "../crawlers") }, (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        console.error("!! Node couldn't execute the command !!");
-        console.log(err);
-        return;
-      }
+    const execDir = path.join(cwd(), "src", "crawlers");
+    console.info("exec dir :", execDir);
 
-      // the *entire* stdout and stderr (buffered)
-      console.info(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    });
+    exec(
+      "python full_weekend.py 2024 canada 1237",
+      { cwd: execDir },
+      (err, stdout, stderr) => {
+        // the *entire* stdout (buffered)
+        console.info(`stdout: ${stdout}`);
+        console.info(`stderr: ${stderr}`);
+
+        if (err) {
+          // node couldn't execute the command
+          console.error("!! Node couldn't execute the command !!");
+          console.log(err);
+          return NextResponse.json(
+            { error: "Internal Server Error", details: err },
+            { status: 500 }
+          );
+        }
+
+        if (stderr && stderr !== "") {
+          // node couldn't execute the command
+          console.error("!! Node couldn't execute the command (stderr) !!");
+          return NextResponse.json(
+            { error: "Internal Server Error", details: stderr },
+            { status: 500 }
+          );
+        }
+      }
+    );
 
     return NextResponse.json(null, { status: 200 });
   } catch (error) {
-    console.error("Error processing request", error);
     return NextResponse.json(
-      { error: "Internal Server Error" },
+      { error: "Internal Server Error", details: error },
       { status: 500 }
     );
   }
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -19,15 +19,22 @@ export function useFetch<T extends any>(): UseFetchReturnType<T> {
     setLoading(true);
     fetch(...args)
       .then((response) => {
+        console.info("useFetch response :", response);
         if (!response.ok) throw Error("Error while fetching data");
         return response.json();
       })
       .then((json) => {
-        if ("error" in json) throw Error("Error while fetching data")
+        console.info("useFetch json :", json);
+        if (json && "error" in json) throw Error("Error while fetching data");
         setData(json as T);
       })
       .catch((err) => {
-        console.error("error", error);
+        console.error(
+          "useFetch has found an error :\n",
+          "for request",
+          args[0],
+          error
+        );
         setError(err);
         setData(null);
       })
