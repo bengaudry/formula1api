60ad7be85985d0ca77d736210ae6fa95300c861a
bengaudry <bengaudry@outlook.fr>
2024-06-30 19:40:52 +0200

    fix

--- src/app/api/run-crawler/route.ts
+++ src/app/api/run-crawler/route.ts
@@ -4,13 +4,13 @@ import { fetchQualifyingResults } from "@/crawlers/scripts/qualifying";
 import { fetchGrid } from "@/crawlers/scripts/grid";
 import { fetchRaceResults } from "@/crawlers/scripts/results";
 
-function extractPracticeNb(crawlerType: Crawler) {
+const extractPracticeNb = (crawlerType: Crawler) => {
   if (crawlerType.startsWith("fp")) {
     const nb = parseInt(crawlerType.replace("fp", ""));
     return [1, 2, 3].includes(nb) ? (nb as 1 | 2 | 3) : undefined;
   }
   return;
-}
+};
 
 async function fetchFullWeekend(params: {
   year: string;
@@ -42,42 +42,42 @@ async function fetchSprintWeekend(params: {
   year: string;
   location: string;
   id: string;
-}) {
+}): Promise<{ error: string } | undefined> {
   // Practice session data
   const practiceResults = await fetchPracticeResults({
     ...params,
     practiceNb: 1,
   });
-  if (!practiceResults) return;
+  if (!practiceResults) return { error: `Practice 1 data not found` };
 
   // Sprint data
   const sprintQualiResults = await fetchQualifyingResults({
     ...params,
     isSprint: true,
   });
-  if (!sprintQualiResults) return;
+  if (!sprintQualiResults) return { error: `Sprint qualifying data not found` };
 
   const sprintGridResults = await fetchGrid({
     ...params,
     isSprint: true,
   });
-  if (!sprintGridResults) return;
+  if (!sprintGridResults) return { error: `Sprint grid not found` };
 
   const sprintResults = await fetchRaceResults({
     ...params,
     isSprint: true,
   });
-  if (!sprintResults) return;
+  if (!sprintResults) return { error: `Sprint results not found` };
 
   // Race data
   const qualiResults = await fetchQualifyingResults(params);
-  if (!qualiResults) return;
+  if (!qualiResults) return { error: `Race qualifying data not found` };
 
   const gridResults = await fetchGrid(params);
-  if (!gridResults) return;
+  if (!gridResults) return { error: `Race grid not found` };
 
   const raceResults = await fetchRaceResults(params);
-  if (!raceResults) return;
+  if (!raceResults) return { error: `Race results not found` };
 }
 
 async function fetchDataWithCrawler(
@@ -98,9 +98,7 @@ async function fetchDataWithCrawler(
         });
         if (!results) return resolve(null);
         return resolve(results);
-      }
-
-      if (["sprint-qualifying", "race-qualifying"].includes(crawler)) {
+      } else if (["sprint-qualifying", "race-qualifying"].includes(crawler)) {
         const results = await fetchQualifyingResults({
           year: year,
           id: id,
@@ -109,9 +107,7 @@ async function fetchDataWithCrawler(
         });
         if (!results) return resolve(null);
         return resolve("Generated data successfully");
-      }
-
-      if (["race-grid", "sprint-grid"].includes(crawler)) {
+      } else if (["race-grid", "sprint-grid"].includes(crawler)) {
         const results = await fetchGrid({
           year: year,
           id: id,
@@ -120,9 +116,7 @@ async function fetchDataWithCrawler(
         });
         if (!results) return resolve(null);
         return resolve("Generated data successfully");
-      }
-
-      if (["race-results", "sprint-results"].includes(crawler)) {
+      } else if (["race-results", "sprint-results"].includes(crawler)) {
         const results = await fetchRaceResults({
           year: year,
           id: id,
@@ -131,19 +125,15 @@ async function fetchDataWithCrawler(
         });
         if (!results) return resolve(null);
         return resolve("Generated data successfully");
-      }
-
-      if (crawler === "full-weekend") {
-        await fetchFullWeekend({ year, id, location });
+      } else if (crawler === "full-weekend") {
+        const data = await fetchFullWeekend({ year, id, location });
+        // if (data && "error" in data) return resolve(data.error);
         return resolve("Generated data successfully");
-      }
-
-      if (crawler === "sprint-weekend") {
-        await fetchSprintWeekend({ year, id, location });
+      } else if (crawler === "sprint-weekend") {
+        const data = await fetchSprintWeekend({ year, id, location });
+        if (data && "error" in data) return resolve(data.error);
         return resolve("Generated data successfully");
-      }
-
-      throw reject("Invalid crawler");
+      } else reject("Crawler is not valid.");
     } catch (err) {
       reject(err);
     }
@@ -177,7 +167,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(results, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { error: "Internal Server Error", details: error },
+      { error: "Internal server err", details: error },
       { status: 500 }
     );
   }
